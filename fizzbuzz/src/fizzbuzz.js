a1ff6700e9b30a6197d5591854350eff61206ed0
unknown <silviaespanagil@gmail.com>
2021-12-01 12:06:17 +0100

    Add test and code

--- fizzbuzz/src/fizzbuzz.js
+++ fizzbuzz/src/fizzbuzz.js
@@ -1,3 +1,18 @@
-export default () => {
-  return true;
-}
+export const fizzbuzz = (num) => {
+  const numberArray = [];
+
+  for (let i = 1; i < 101; i++) {
+    numberArray.push(works(i));
+  }
+  return numberArray;
+
+  function works(num) {
+    if (num % 3 === 0 && num % 5 === 0) {
+      return "FizzBuzz";
+    } else if (num % 3 === 0 || num.toString().indexOf("3") > -1) {
+      return "Fizz";
+    } else if (num % 5 === 0 || num.toString().indexOf("5") > -1) {
+      return "Buzz";
+    } else return num;
+  }
+};
--- fizzbuzz/src/fizzbuzz.test.js
+++ fizzbuzz/src/fizzbuzz.test.js
@@ -1,5 +1,25 @@
-import fizzbuzz from "./fizzbuzz";
+import { fizzbuzz } from "./fizzbuzz";
 
-test("canary test", () => {
-  expect(fizzbuzz()).toEqual(true);
+test("test value number", () => {
+  expect(fizzbuzz()[0]).toEqual(1);
+});
+
+test("test Fizz", () => {
+  expect(fizzbuzz()[2]).toEqual("Fizz");
+});
+
+test("test Buzz", () => {
+  expect(fizzbuzz()[4]).toEqual("Buzz");
+});
+
+test("test FizzBuzz", () => {
+  expect(fizzbuzz()[14]).toEqual("FizzBuzz");
+});
+
+test("test Fizz with number contain", () => {
+  expect(fizzbuzz()[52]).toEqual("Fizz");
+});
+
+test("test Buzz with number contain", () => {
+  expect(fizzbuzz()[54]).toEqual("Buzz");
 });
