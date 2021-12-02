573d570b144b383f009c934412932fe6478046e4
unknown <silviaespanagil@gmail.com>
2021-12-02 11:23:34 +0100

    Solve Password validator Kata

--- password-validator/src/passwordValidator.js
+++ password-validator/src/passwordValidator.js
@@ -1,3 +1,8 @@
-export default () => {
-  return true;
+export function passwordCheck (pass) {
+  let regex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[_]).{9,32}$/
+  
+  if (!regex.test(pass)) {
+    return false
+  }
+  return true
 }
--- password-validator/src/passwordValidator.test.js
+++ password-validator/src/passwordValidator.test.js
@@ -1,5 +1,25 @@
-import passwordValidator from "./passwordValidator";
+import { passwordCheck } from "./passwordValidator";
 
-test("canary test", () => {
-  expect(false).toEqual(true);
+test("valid password", () => {
+  expect(passwordCheck("123456Pe_")).toEqual(true);
+});
+
+test("password length valid", () => {
+  expect(passwordCheck("12345Pe_")).toEqual(false);
+});
+
+test("password contains underscore", () => {
+  expect(passwordCheck("1234567Pe")).toEqual(false);
+});
+
+test("password contains capital letter", () => {
+  expect(passwordCheck("123456pe_")).toEqual(false);
+});
+
+test("password contains lowercase", () => {
+  expect(passwordCheck("123456PE_")).toEqual(false);
+});
+
+test("password contains at least one number", () => {
+  expect(passwordCheck("Pepinillo_")).toEqual(false);
 });
